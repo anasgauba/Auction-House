573ccb9aedd2f6d338df7e7f6a1bd7c69455ab7a
Nathan <ildledx@gmail.com>
2018-11-18 17:47:41 -0700

    Updated Agent Display with a few features and fixes
    Buttons no longer populate in the table rows
    all item info now displayed on left
    Bid button and bid amount now on left

--- src/Agent_Display.java
+++ src/Agent_Display.java
@@ -15,16 +15,19 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.text.DecimalFormat;
+import java.util.Stack;
 
 public class Agent_Display extends JPanel {
     private ObservableList<tableItem> listofTableItems = FXCollections.observableArrayList();
     private ObservableList<String> listOfHouses = FXCollections.observableArrayList();
 
-    private TableView table = new TableView();
+    private TableView<tableItem> table = new TableView();
     
     public void drawGUI(Stage stage) {
         BorderPane agentInfo = new BorderPane();
-        agentInfo.setPadding(new Insets(20, 20, 20, 20));
+        agentInfo.setPadding(new Insets(23, 20, 20, 20));
 
         Label agentID = new Label("Agent ID: 3429234");
         agentID.setFont(new Font("Calibri", 20));
@@ -39,19 +42,50 @@ public class Agent_Display extends JPanel {
         agentBalance.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
 
         VBox agentLabels = new VBox(agentID, agentAccount, agentBalance);
-        /*agentLabels.setSpacing(10);
-        Button quitSim = new Button("Quit");
-        quitSim.setFont(new Font("Calibri", 10));
-        quitSim.setStyle("-fx-font-weight: bold");
-        quitSim.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        */
+
+        BorderPane itemInfo = new BorderPane();
+        itemInfo.setPadding(new Insets(20, 20, 5, 20));
+
+        Label itemIDLabel = new Label("Item ID:");
+        itemIDLabel.setFont(new Font("Calibri", 20));
+        itemIDLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+
+        Label itemDescriptionLabel = new Label("Item Name:");
+        itemDescriptionLabel.setFont(new Font("Calibri", 20));
+        itemDescriptionLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+
+        Label itemStartingBidLabel = new Label("Starting Bid:");
+        itemStartingBidLabel.setFont(new Font("Calibri", 20));
+        itemStartingBidLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+
+        Label itemCurrentBidLabel = new Label("Current Bid:");
+        itemCurrentBidLabel.setFont(new Font("Calibri", 20));
+        itemCurrentBidLabel.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
+
+        VBox itemLabels = new VBox(itemDescriptionLabel, itemIDLabel, itemStartingBidLabel, itemCurrentBidLabel);
+
+
         agentInfo.setTop(agentLabels);
-        //agentInfo.setBottom(quitSim);
+        itemInfo.setTop(itemLabels);
+
+        TextField bidAmount = new TextField("0.00");
+        bidAmount.setPrefWidth(100);
+        bidAmount.setAlignment(Pos.CENTER_RIGHT);
+        bidAmount.setFocusTraversable(false);
+        bidAmount.setOnMouseClicked(event -> {
+            bidAmount.setText("");
+        });
+
+        Button placeBid = new Button("Place Bid");
+        placeBid.setPrefWidth(75);
+
+        HBox bidStackHBox = new HBox();
+        bidStackHBox.getChildren().addAll(bidAmount, placeBid);
+        bidStackHBox.setPadding(new Insets(0, 0, 20, 20));
+        bidStackHBox.setSpacing(10);
+
+
+
 
         VBox auctionHouseBox = new VBox();
         auctionHouseBox.setPadding(new Insets(20, 20, 20, 20));
@@ -60,13 +94,6 @@ public class Agent_Display extends JPanel {
         auctionHouseChoice.setFont(new Font("Calibri", 25));
         auctionHouseChoice.setStyle("-fx-font-weight: bold; -fx-text-fill: white");
 
-        /*
-        ChoiceBox<String> auctionHouseList = new ChoiceBox<>();
-        auctionHouseList.setMinWidth(100);
-        listOfHouses.addAll("House 1", "House 2");
-        auctionHouseList.setItems(listOfHouses);
-        auctionHouseList.getSelectionModel().selectFirst();
-        */
 
         ObservableList<String> options =
                 FXCollections.observableArrayList(
@@ -96,13 +123,12 @@ public class Agent_Display extends JPanel {
         itemStartingBid.setMinWidth(150);
         TableColumn itemCurrentBid = new TableColumn("Item Current Bid");
         itemCurrentBid.setMinWidth(150);
-        TableColumn itemPlaceBid = new TableColumn("Item Place Bid");
-        itemPlaceBid.setMinWidth(100);
         itemID.setCellValueFactory(
                 new PropertyValueFactory<tableItem, String>("itemID")
         );
         itemName.setCellValueFactory(
                 new PropertyValueFactory<tableItem, String>("itemName")
+
         );
         itemStartingBid.setCellValueFactory(
                 new PropertyValueFactory<tableItem, String>("itemStartingBid")
@@ -110,25 +136,42 @@ public class Agent_Display extends JPanel {
         itemCurrentBid.setCellValueFactory(
                 new PropertyValueFactory<tableItem, String>("itemCurrentBid")
         );
-        itemPlaceBid.setCellValueFactory(
-                new PropertyValueFactory<tableItem, String>("itemPlaceBid")
-        );
 
         //A list of items will be passed in. In final product, I will loop through the list and pass each item
         //to the constructor of new tableItem, which sets all the correct info to the right places on table view. Then,
         // the table data is set. To change list on board, you clear listofTableItems, add table items to it, then set the
         //info in the table to that.
-        Item first = new Item("3333","22222","red table",400,330);
-        Item second = new Item("666","9999","orange mat",200,100);
-        Item third = new Item("4444","11111","green grass",20,10);
+        Item first = new Item("3333","22222","red table",400,330.75);
+        Item second = new Item("666","9999","orange mat",200,100.02);
+        Item third = new Item("4444","11111","green grass",20,10.20);
         listofTableItems.add(new tableItem(first));
         listofTableItems.add(new tableItem(second));
         listofTableItems.add(new tableItem(third));
         table.setItems(listofTableItems);
-        table.getColumns().addAll(itemID, itemName, itemStartingBid, itemCurrentBid, itemPlaceBid);
+        table.getColumns().addAll(itemID, itemName, itemStartingBid, itemCurrentBid);
 
+
+        table.setRowFactory(tv -> {
+            TableRow row = new TableRow();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty()) {
+                    DecimalFormat tempFormat = new DecimalFormat("#.00");
+                    tableItem rowData = (tableItem) row.getItem();
+                    itemDescriptionLabel.setText("Item Name: " + rowData.getItemName());
+                    itemIDLabel.setText("Item ID: " + rowData.getItemID());
+                    itemStartingBidLabel.setText("Starting Bid: " + rowData.getItemStartingBid());
+                    itemCurrentBidLabel.setText("Current Bid: " + rowData.getItemCurrentBid());
+                    double tempValue = Double.valueOf(rowData.getItemCurrentBid()) + 1.00;
+                    bidAmount.setText(String.valueOf(tempFormat.format(tempValue)));
+                }
+            });
+            return row;
+        });
+
+        VBox leftVBox = new VBox();
+        leftVBox.getChildren().addAll(agentInfo, itemInfo, bidStackHBox);
         BorderPane borderpane = new BorderPane();
-        borderpane.setLeft(agentInfo);
+        borderpane.setLeft(leftVBox);
         borderpane.setCenter(auctionHouseBox);
         borderpane.setBackground(new Background(new BackgroundFill(Color.rgb(54, 69, 79), CornerRadii.EMPTY, Insets.EMPTY)));
         Scene scene = new Scene(borderpane, 1300, 600);
@@ -142,29 +185,14 @@ public class Agent_Display extends JPanel {
         private final SimpleStringProperty itemName;
         private final SimpleStringProperty itemStartingBid;
         private final SimpleStringProperty itemCurrentBid;
-        private Button itemPlaceBid;
+        DecimalFormat tempFormat = new DecimalFormat("#.00");
 
         public tableItem(Item itemPassedIn) {
+
             this.itemID = new SimpleStringProperty(itemPassedIn.getItemID());
             this.itemName = new SimpleStringProperty(itemPassedIn.getDescription());
-            this.itemStartingBid = new SimpleStringProperty(Double.toString(itemPassedIn.getMinimumBidAmount()));
-            this.itemCurrentBid = new SimpleStringProperty(Double.toString(itemPassedIn.getCurrentBidAmount()));
-            this.itemPlaceBid = new Button("Place a bet");
-            itemPlaceBid.setOnAction(e -> {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Make your Bet");
-                alert.setHeaderText("Please place your bet in the text field:");
-                GridPane biddingPane = new GridPane();
-                TextField enterBid = new TextField();
-                enterBid.setPromptText("Enter Your Bet");
-                enterBid.setPrefColumnCount(10);
-                enterBid.getText();
-                biddingPane.add(enterBid,0,0);
-                GridPane.setConstraints(enterBid, 0, 0);
-                alert.getDialogPane().setContent(biddingPane);
-                alert.showAndWait();
-            });
-
+            this.itemStartingBid = new SimpleStringProperty(tempFormat.format(itemPassedIn.getMinimumBidAmount()));
+            this.itemCurrentBid = new SimpleStringProperty(tempFormat.format(itemPassedIn.getCurrentBidAmount()));
         }
 
         public String getItemID() {
@@ -197,14 +225,6 @@ public class Agent_Display extends JPanel {
 
         public void setItemCurrentBid(String itemCurrentBidString) {
             itemStartingBid.set(itemCurrentBidString);
-        }
-
-        public Button getItemPlaceBid() {
-            return itemPlaceBid;
-        }
-
-        public void setItemPlaceBid(Button itemPlaceBidString) {
-            this.itemPlaceBid=itemPlaceBidString;
         }
     }
 }
