b871bdd9a8b48cd5f1f9b69c1dcd9f8c346df0dd
Anas Gauba <anas.farooq71@gmail.com>
2018-11-27 11:09:21 -0700

    Enum class

---
+++ src/Command.java
@@ -0,0 +1,5 @@
+public enum Command {
+    GetListHouses, GetListItems, CloseBankAccount,TransferBlockedFunds,
+    CreateBankAccount, BlockFunds, WinMessage, BidOvertaken, AcceptResponse,
+    RejectResponse
+}
